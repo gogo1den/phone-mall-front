7e1d3c26e0353136e139342c0b2a7b3fb18c1ff9
GOEUN PARK <dbtshfdl1@gmail.com>
2023-05-15 22:20:01 +0900

    제품 테이블+제품 추가 완료

---
+++ src/ProductTable.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button } from "@material-ui/core"
+
+
+class ProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { item: props.item, readOnly: true };
+        this.delete = props.delete;
+    }
+
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
+    }
+
+
+    render() {
+        const item = this.state.item;
+        return(
+            <tr>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td><Button
+                    onClick={this.deleteEventHandler}>
+                    delete</Button>
+                  </td>
+            </tr>
+
+        );
+    }
+}
+
+export default ProductTable;
